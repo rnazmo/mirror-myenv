c6e9877d6ab5115c98ef79caaf17fcfef11da171
rnazmo <rnazmo@gmail.com>
2024-09-13 22:14:36 +0000

    Update file setup.arch_based.x64.bash

--- modules/10_core/cli/ghq/setup.arch_based.x64.bash
+++ modules/10_core/cli/ghq/setup.arch_based.x64.bash
@@ -2,15 +2,22 @@
 set -eu
 
 main() {
+    install
+    # config
+    # install
+    # upgrade
+    # symlink
+    # uninstall
+}
+
+install() {
+    local -r CMD_NAME="ghq"
+    local -r RESOURCE="x-motemen/ghq"
     if !check_if_command_exists "ghq"; then
-        install_via_aqua
+        install_via_aqua "x-motemen/ghq"
     fi
 }
 
 main
 
 
-# install
-# upgrade
-# symlink
-# uninstall
