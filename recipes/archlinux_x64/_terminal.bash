359b2a1c43903f655cfdb304fb19b84778612e2c
rnazmo <rnazmo@gmail.com>
2024-09-17 21:12:11 +0000

    small refactor

--- recipes/archlinux_x64/_terminal.bash
+++ recipes/archlinux_x64/_terminal.bash
@@ -15,14 +15,17 @@ main() {
     # ======================================================
 
     # ======== zsh
-    local -r ZDOTDIR="${HOME}/.config/zsh"
     sudo pacman -S --needed zsh
-    [ -L "${HOME}/.zshrc" ] && unlink "${HOME}/.zshrc"
-    [ -f "${HOME}/.zshrc" ] && mv "${HOME}/.zshrc" "${HOME}/.zshrc.old"
+
+    local -r ZDOTDIR="${HOME}/.config/zsh"
+    local -r ZSH_COMPLETION_DIR="${ZDOTDIR}/completion"
+
+    [ -L "${HOME}/.zshrc" ]  && unlink "${HOME}/.zshrc"
     [ -L "${HOME}/.zshenv" ] && unlink "${HOME}/.zshenv"
+    [ -f "${HOME}/.zshrc" ]  && mv "${HOME}/.zshrc"  "${HOME}/.zshrc.old"
     [ -f "${HOME}/.zshenv" ] && mv "${HOME}/.zshenv" "${HOME}/.zshenv.old"
     link_file "${MYENV_ROOT}/config/home/.zshenv" "${HOME}/.zshenv"
-    link_dir "${MYENV_ROOT}/config/home/.config/zsh" "${HOME}/.config/zsh" # NOTE: the path is directory
+    link_dir "${MYENV_ROOT}/config/home/.config/zsh" "$ZDOTDIR" # NOTE: the path is directory
 
     # ======== sheldon
     # sudo pacman -S --needed sheldon
@@ -36,8 +39,7 @@ main() {
     #     https://github.com/git/git/blob/ed155187b429a2a6b6475efe1767053df37ccfe1/contrib/completion/git-completion.zsh
     #     [【初級者向け】ターミナル（zsh）にgitのブランチ名表示＆補完機能を公式ドキュメントをじっくり読み解いて実装 - Qiita]
     #         (https://qiita.com/yamaday0u/items/ee8acb35709bcc8c7fc7#tab%E8%A3%9C%E5%AE%8C%E6%A9%9F%E8%83%BD%E3%82%92%E5%AE%9F%E8%A3%85git-completion)
-    # TODO: This work????
-    local -r ZSH_COMPLETION_DIR="${ZDOTDIR}/completion"
+    # TODO: Is this working???
     if [ ! -f "${ZSH_COMPLETION_DIR}/_git" ] || [ ! -f "${ZSH_COMPLETION_DIR}/git-completion.bash" ]; then
         mkdir -p "$ZSH_COMPLETION_DIR"
         curl -o "${ZSH_COMPLETION_DIR}/_git"                https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
