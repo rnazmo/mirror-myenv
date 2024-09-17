eeb9648b026346f3b353b915afff0dc29dabf2d7
rnazmo <rnazmo@gmail.com>
2024-09-17 04:39:53 +0000

    Update file setup.bash

--- recipes/archlinux_x64/setup.bash
+++ recipes/archlinux_x64/setup.bash
@@ -12,7 +12,7 @@ main() {
     source "${MYENV_ROOT}/recipes/archlinux_x64/_devel.bash"
 
     source "${MYENV_ROOT}/recipes/archlinux_x64/_editor.bash"
-    # source "${MYENV_ROOT}/recipes/archlinux_x64/_browser.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_browser.bash"
 
     # is_virtualbox_guest && source "${MYENV_ROOT}/recipes/archlinux_x64/_devel.ve_guest_vbox.bash"
     # is_not_virtualized_environment && source "${MYENV_ROOT}/recipes/archlinux_x64/_devel.ve_host.bash"
