19e74bbefaeb5800bc5e232b9ba6cfcfcc65f7b5
rnazmo <rnazmo@gmail.com>
2024-09-17 01:44:36 +0000

    debugging

--- hosts/soba/setup.bash
+++ hosts/soba/setup.bash
@@ -4,9 +4,7 @@ set -eu
 source ${MYENV_ROOT}/lib/util.bash
 
 main() {
-    log_debug "Hello! from soba/setup.bash"
-
-    # source ${MYENV_ROOT}/recipes/archlinux_x64/setup.bash
+    source ${MYENV_ROOT}/recipes/archlinux_x64/setup.bash
 }
 
 main
