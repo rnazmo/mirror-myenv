e66dde1ee602a11b7fb0b785aec5bab163a642cf
rnazmo <rnazmo@gmail.com>
2024-09-17 03:27:46 +0000

    fix

--- recipes/archlinux_x64/_base.bash
+++ recipes/archlinux_x64/_base.bash
@@ -4,7 +4,7 @@ set -eu
 source "${MYENV_ROOT}/lib/util.bash"
 
 main() {
-    log_debug "START: $(basename $0)"
+    log_debug "START: ${BASH_SOURCE##*/}"
 
     # ======================================================
     # ======== system update                               =
@@ -81,7 +81,7 @@ main() {
     mkdir -p -v ~/workspace/sandboxes
     mkdir -p -v ~/workspace/temp
 
-    log_debug "END  : $(basename $0)"
+    log_debug "END  : ${BASH_SOURCE##*/}"
 }
 
 main
