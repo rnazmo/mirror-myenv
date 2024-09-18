585e0db98a478274ff3c93c6e71120aeba777f2d
rnazmo <rnazmo@gmail.com>
2024-09-18 02:31:33 +0000

    fix typo

--- recipes/archlinux_x64/_terminal.bash
+++ recipes/archlinux_x64/_terminal.bash
@@ -62,7 +62,7 @@ main() {
 
     sudo pacman -S --needed alacritty
     link_file "${MYENV_ROOT}/config/home/.config/alacritty/alacritty.toml" "${HOME}/.config/alacritty/alacritty.toml"
-    link_file "${MYENV_ROOT}/config/home/.config/alacritty/my-themetoml"   "${HOME}/.config/alacritty/my-theme.toml"
+    link_file "${MYENV_ROOT}/config/home/.config/alacritty/my-theme.toml"   "${HOME}/.config/alacritty/my-theme.toml"
 
     # ======================================================
     # ======== starship (& theme)                          =
