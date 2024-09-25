fb8dd2506021ffc0c1c06aaeed827de85c556a80
rnazmo <rnazmo@gmail.com>
2024-09-25 14:14:35 +0000

    small fix

--- recipes/archlinux_x64/_terminal.bash
+++ recipes/archlinux_x64/_terminal.bash
@@ -20,10 +20,10 @@ main() {
     local -r ZDOTDIR="${HOME}/.config/zsh"
     local -r ZSH_COMPLETION_DIR="${ZDOTDIR}/completion"
 
-    [ -L "${HOME}/.zshrc" ]  && unlink "${HOME}/.zshrc"
+    [ -L "${HOME}/.zshrc"  ] && unlink "${HOME}/.zshrc"
     [ -L "${HOME}/.zshenv" ] && unlink "${HOME}/.zshenv"
-    [ -f "${HOME}/.zshrc" ]  && mv "${HOME}/.zshrc"  "${HOME}/.zshrc.old"
-    [ -f "${HOME}/.zshenv" ] && mv "${HOME}/.zshenv" "${HOME}/.zshenv.old"
+    [ -f "${HOME}/.zshrc"  ] && mv     "${HOME}/.zshrc"  "${HOME}/.zshrc.old"
+    [ -f "${HOME}/.zshenv" ] && mv     "${HOME}/.zshenv" "${HOME}/.zshenv.old"
     link_file "${MYENV_ROOT}/config/home/.zshenv"     "${HOME}/.zshenv"
     link_dir  "${MYENV_ROOT}/config/home/.config/zsh" "$ZDOTDIR" # NOTE: the path is directory
 
@@ -41,9 +41,9 @@ main() {
     #         (https://qiita.com/yamaday0u/items/ee8acb35709bcc8c7fc7#tab%E8%A3%9C%E5%AE%8C%E6%A9%9F%E8%83%BD%E3%82%92%E5%AE%9F%E8%A3%85git-completion)
     # TODO: Is this working???
     if [ ! -f "${ZSH_COMPLETION_DIR}/_git" ] || [ ! -f "${ZSH_COMPLETION_DIR}/git-completion.bash" ]; then
-        mkdir -p "$ZSH_COMPLETION_DIR"
-        curl -o "${ZSH_COMPLETION_DIR}/_git"                https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
-        curl -o "${ZSH_COMPLETION_DIR}/git-completion.bash" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+        mkdir -p "${ZSH_COMPLETION_DIR}"
+        curl -o  "${ZSH_COMPLETION_DIR}/_git"                https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+        curl -o  "${ZSH_COMPLETION_DIR}/git-completion.bash" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
     fi
 
     # ======================================================
@@ -62,7 +62,7 @@ main() {
 
     sudo pacman -S --needed alacritty
     link_file "${MYENV_ROOT}/config/home/.config/alacritty/alacritty.toml" "${HOME}/.config/alacritty/alacritty.toml"
-    link_file "${MYENV_ROOT}/config/home/.config/alacritty/my-theme.toml"   "${HOME}/.config/alacritty/my-theme.toml"
+    link_file "${MYENV_ROOT}/config/home/.config/alacritty/my-theme.toml"  "${HOME}/.config/alacritty/my-theme.toml"
 
     # ======================================================
     # ======== starship (& theme)                          =
