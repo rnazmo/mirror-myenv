75a16f6e4f9f293107a209fe07bd8da81884f8ea
rnazmo <rnazmo@gmail.com>
2024-10-03 21:54:47 +0900

    feat(neovim): Setup Codeium Chat

--- config/home/.config/nvim/lua/rnazmo/plugins/codeium.lua
+++ config/home/.config/nvim/lua/rnazmo/plugins/codeium.lua
@@ -5,6 +5,10 @@ return {
     "hrsh7th/nvim-cmp",
   },
   config = function()
-    require("codeium").setup({})
+    require("codeium").setup({
+      -- To use codeium chat with `:Codeium Chat` command.
+      -- Ref: https://github.com/Exafunction/codeium.nvim/blob/17bbefff02be8fd66931f366bd4ed76a76e4a57e/README.md#usage
+      enable_chat = true,
+    })
   end,
 }
