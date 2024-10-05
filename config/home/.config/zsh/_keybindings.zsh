6e3022fa23ed99df3cfea559e7089de8d7af0a68
rnazmo <rnazmo@gmail.com>
2024-10-06 00:11:13 +0900

    fix(zsh): the keybinding (Ctrl-q) not work

--- config/home/.config/zsh/_keybindings.zsh
+++ config/home/.config/zsh/_keybindings.zsh
@@ -16,8 +16,33 @@ bindkey '^[[Z' undo                               # shift + tab undo last action
 
 # ======== my functions
 
+# What is this:
+#     Search Git repositories incrementally with preview
+#
+# Dependencies:
+#     fzf, ghq
+#
+# Keybinding:
+#     `Ctrl-g`
+#
+# Ref:
+#     [ghq で管理しているリポジトリに Ctrl-g で cd する on zsh - sheepla/ghq-fzf.zsh]
+#         (https://gist.github.com/sheepla/d680f1480d8c36c4290d6aabebf1abc6)
+function _fuzzy_choose_repo() {
+    FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --reverse --height=50%"
+    local -r root="$(ghq root)"
+    local -r repo="$(ghq list | fzf --preview="ls -AF --color=always ${root}/{1}")"
+    local -r dir="${root}/${repo}"
+    if [ -z "${dir}" ]; then
+        echo "no directories found"
+        return 1
+    fi
+    cd "${dir}"
+    zle accept-line
+    zle reset-prompt
+}
 zle -N _fuzzy_choose_repo
-bindkey "^q" _fuzzy_choose_repo
+bindkey "^Xq" _fuzzy_choose_repo
 
 # ======== fzf
 if [[ ! -f "${ZSH_KEYBINDINGS_LOCAL_DIR}/key-bindings.zsh" ]]; then
