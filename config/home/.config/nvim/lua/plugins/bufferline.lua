bad010462c88871999161ddcdfe05ddca5e192f3
rnazmo <rnazmo@gmail.com>
2024-10-10 19:09:25 +0900

    feat(neovim): Customize bufferline config
    Use "tabs" mode (= disable buffer display), because buffer are hard to
    manage for me.

---
+++ config/home/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      -- Buffers are hard to manage, so I don't use them.
+      mode = "tabs",
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+
+      always_show_bufferline = true,
+    },
+  },
+}
