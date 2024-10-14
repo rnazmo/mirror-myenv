ff7005c14880bd16bfbca741a236c19db6bd82f1
rnazmo <rnazmo@gmail.com>
2024-10-14 23:24:07 +0900

    perf(myenv): Disable unused editor (VS Code)

--- recipes/archlinux_x64/_editor.bash
+++ recipes/archlinux_x64/_editor.bash
@@ -20,32 +20,33 @@ main() {
     # Sync (= install & cleanup & update) plugins
     nvim --headless "+Lazy! sync" +qa
 
-    # ======================================================
-    # ======== vscode (& extensions)                       =
-    # ======================================================
-
-    yay -S --needed --noconfirm visual-studio-code-bin
-    link_file "${MYENV_ROOT}/config/home/.config/Code/User/settings.json" "${HOME}/.config/Code/User/settings.json"
-    link_file "${MYENV_ROOT}/config/home/.config/Code/User/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
-    link_dir "${MYENV_ROOT}/config/home/.config/Code/User/snippets" "${HOME}/.config/Code/User/snippets" # NOTE: dir
-
-    # Install extensions
-    EXTENSIONS=(
-        "golang.Go"
-        # "ms-azuretools.vscode-docker"
-        # "ms-vscode-remote.remote-containers"
-        # "GitHub.copilot"
-        # "Codeium.codeium"
-        "editorconfig.editorconfig"
-        # "esbenp.prettier-vscode"
-        # "dbaeumer.vscode-eslint"
-        # "streetsidesoftware.code-spell-checker"
-        # "eamodio.gitlens"
-        # "donjayamanne.githistory"
-    )
-    for EXT in "${EXTENSIONS[@]}"; do
-        code --install-extension "$EXT"
-    done
+    # # ======================================================
+    # # ======== vscode (& extensions)                       =
+    # # ======================================================
+    #
+    # yay -S --needed --noconfirm visual-studio-code-bin
+    # link_file "${MYENV_ROOT}/config/home/.config/Code/User/settings.json" "${HOME}/.config/Code/User/settings.json"
+    # link_file "${MYENV_ROOT}/config/home/.config/Code/User/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
+    # link_dir "${MYENV_ROOT}/config/home/.config/Code/User/snippets" "${HOME}/.config/Code/User/snippets" # NOTE: dir
+    #
+    # # Install extensions
+    # EXTENSIONS=(
+    #     # TODO: Use "VSCode Neovim"
+    #     "golang.Go"
+    #     # "ms-azuretools.vscode-docker"
+    #     # "ms-vscode-remote.remote-containers"
+    #     # "GitHub.copilot"
+    #     # "Codeium.codeium"
+    #     "editorconfig.editorconfig"
+    #     # "esbenp.prettier-vscode"
+    #     # "dbaeumer.vscode-eslint"
+    #     # "streetsidesoftware.code-spell-checker"
+    #     # "eamodio.gitlens"
+    #     # "donjayamanne.githistory"
+    # )
+    # for EXT in "${EXTENSIONS[@]}"; do
+    #     code --install-extension "$EXT"
+    # done
 
     # ======================================================
     # ======== obsidian                                    =
