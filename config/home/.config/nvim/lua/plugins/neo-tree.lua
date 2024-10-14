aea0da7133c4991c5eb41c4c3206eb89f814e183
rnazmo <rnazmo@gmail.com>
2024-10-14 18:20:39 +0900

    feat(neovim): Add keybindings for neo-tree (focus)
    Override some neo-tree default keybindings in Lazyvim.

---
+++ config/home/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<leader>e", "<cmd>Neotree focus<cr>", desc = "Focus NeoTree", remap = true },
+    { "<leader>E", "<cmd>Neotree close<cr>", desc = "Close NeoTree", remap = true },
+  },
+}
