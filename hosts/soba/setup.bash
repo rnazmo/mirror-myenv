74f8254c9a1c44c5307f5dd2250eb59f31301782
rnazmo <rnazmo@gmail.com>
2024-10-15 18:01:55 +0900

    refactor(myenv): Rethink recipe categories

--- hosts/soba/setup.bash
+++ hosts/soba/setup.bash
@@ -3,9 +3,9 @@ set -eu
 
 source "${MYENV_ROOT}/lib/util.bash"
 
-source "${MYENV_ROOT}/recipes/archlinux_x64/core.bash"
-source "${MYENV_ROOT}/recipes/archlinux_x64/base.bash"
-source "${MYENV_ROOT}/recipes/archlinux_x64/extra.bash"
+source "${MYENV_ROOT}/recipes/manjaro_x64/core.bash"
+source "${MYENV_ROOT}/recipes/manjaro_x64/base.bash"
+source "${MYENV_ROOT}/recipes/manjaro_x64/extra.bash"
 
 main() {
     log_debug "START: ${BASH_SOURCE##*/}"
