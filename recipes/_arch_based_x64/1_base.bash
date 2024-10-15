725705fa1426e4737c1b8225a1fb4e311155fb73
rnazmo <rnazmo@gmail.com>
2024-10-15 19:46:36 +0900

    refactor(myenv): Split the func

--- recipes/_arch_based_x64/1_base.bash
+++ recipes/_arch_based_x64/1_base.bash
@@ -65,7 +65,36 @@ _setup_ime() {
     :
 }
 
-_setup_shell() {
+_setup_util() {
+    log_debug "START: ${BASH_SOURCE}"
+
+    # ======================================================
+    # ======== many cli tools                              =
+    # ======================================================
+
+    sudo pacman -S --needed --noconfirm \
+        ghq fzf tree xclip unzip \
+        ripgrep bat eza fd bottom
+
+    # ======== fastfetch
+    sudo pacman -S --needed --noconfirm fastfetch
+    remove_unused_config "${HOME}/.config/fastfetch/config.jsonc"
+    link_file "${MYENV_ROOT}/config/home/.config/fastfetch/config.jsonc" "${HOME}/.config/fastfetch/config.jsonc"
+
+    # ======== proper7y
+    if ! check_if_command_exists "proper7y"; then
+        local -r dest_dir="${HOME}/bin"
+
+        cd "$(mktemp -d)"
+        curl -O https://raw.githubusercontent.com/rnazmo/proper7y/main/install.bash
+        chmod +x ./install.bash
+        ./install.bash "$dest_dir"
+    fi
+
+    log_debug "END  : ${BASH_SOURCE}"
+}
+
+setup_shell() {
     log_debug "START: ${BASH_SOURCE}"
 
     sudo pacman -Syu --noconfirm
@@ -272,36 +301,17 @@ _setup_multiplexer() {
 _setup_devel() {
     log_debug "START: ${BASH_SOURCE}"
 
-    sudo pacman -Syu --noconfirm
-
     # ======================================================
     # ======== many cli tools                              =
     # ======================================================
 
     sudo pacman -S --needed --noconfirm \
-        ghq fzf tree xclip unzip \
-        ripgrep bat eza fd git-delta bottom \
+        lazygit git-delta \
         shellcheck shfmt
 
-    # ======== proper7y
-    if ! check_if_command_exists "proper7y"; then
-        local -r dest_dir="${HOME}/bin"
-
-        cd "$(mktemp -d)"
-        curl -O https://raw.githubusercontent.com/rnazmo/proper7y/main/install.bash
-        chmod +x ./install.bash
-        ./install.bash "$dest_dir"
-    fi
-
     # ======== lazygit
-    sudo pacman -S --needed --noconfirm lazygit
     remove_unused_config "${HOME}/.config/lazygit/config.yml"
     link_file "${MYENV_ROOT}/config/home/.config/lazygit/config.yml" "${HOME}/.config/lazygit/config.yml"
-
-    # ======== fastfetch
-    sudo pacman -S --needed --noconfirm fastfetch
-    remove_unused_config "${HOME}/.config/fastfetch/config.jsonc"
-    link_file "${MYENV_ROOT}/config/home/.config/fastfetch/config.jsonc" "${HOME}/.config/fastfetch/config.jsonc"
 
     log_debug "END  : ${BASH_SOURCE}"
 }
