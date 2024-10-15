7d955befb01bea222f0adfdaaa501a9c364c27dc
rnazmo <rnazmo@gmail.com>
2024-10-15 18:25:32 +0900

    refactor(myenv): In manjaro_x64 recipes, import _arch_based_x64 recipes

---
+++ recipes/_arch_based_x64/2_extra.bash
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env bash
+set -eu
+
+# virtualized environment guest
+
+source "${MYENV_ROOT}/lib/util.bash"
+
+setup_virtualbox() {
+    log_debug "START: ${BASH_SOURCE}"
+
+    # =============== docker
+    sudo pacman -S docker docker-compose
+
+    # =============== virtualbox (host)
+    sudo pacman -S --needed virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
+    sudo gpasswd -a $USER vboxusers
+    sudo modprobe vboxdrv
+
+    log_debug "END  : ${BASH_SOURCE}"
+}
+
+setup_virtualbox_guest() {
+    log_debug "START: ${BASH_SOURCE}"
+
+    # =============== virtualbox (guest)
+    # Ref: https://wiki.archlinux.org/title/VirtualBox/Install_Arch_Linux_as_a_guest
+    sudo pacman -S --needed virtualbox-guest-utils
+
+    log_debug "END  : ${BASH_SOURCE}"
+}
--- recipes/manjaro_x64/2_extra.bash
+++ recipes/manjaro_x64/2_extra.bash
@@ -1,30 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-# virtualized environment guest
-
-source "${MYENV_ROOT}/lib/util.bash"
-
-setup_virtualbox() {
-    log_debug "START: ${BASH_SOURCE}"
-
-    # =============== docker
-    sudo pacman -S docker docker-compose
-
-    # =============== virtualbox (host)
-    sudo pacman -S --needed virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
-    sudo gpasswd -a $USER vboxusers
-    sudo modprobe vboxdrv
-
-    log_debug "END  : ${BASH_SOURCE}"
-}
-
-setup_virtualbox_guest() {
-    log_debug "START: ${BASH_SOURCE}"
-
-    # =============== virtualbox (guest)
-    # Ref: https://wiki.archlinux.org/title/VirtualBox/Install_Arch_Linux_as_a_guest
-    sudo pacman -S --needed virtualbox-guest-utils
-
-    log_debug "END  : ${BASH_SOURCE}"
-}
+source "${MYENV_ROOT}/recipes/_arch_based_x64/2_extra.bash"
