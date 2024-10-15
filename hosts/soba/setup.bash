1a6475f767c13584a6a4e894c34ea91e64764569
rnazmo <rnazmo@gmail.com>
2024-10-15 17:22:00 +0900

    refactor(myenv): Remove `recipes/archlinux_x64/setup.bash`

--- hosts/soba/setup.bash
+++ hosts/soba/setup.bash
@@ -6,7 +6,13 @@ source "${MYENV_ROOT}/lib/util.bash"
 main() {
     log_debug "START: ${BASH_SOURCE##*/}"
 
-    source "${MYENV_ROOT}/recipes/archlinux_x64/setup.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_base.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_fonts.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_terminal.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_devel.bash"
+
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_editor.bash"
+    source "${MYENV_ROOT}/recipes/archlinux_x64/_browser.bash"
 
     log_debug "END  : ${BASH_SOURCE##*/}"
 }
