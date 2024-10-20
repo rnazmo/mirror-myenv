1c56bd46d46afc12a38577db9a2e73fc6726f819
rnazmo <rnazmo@gmail.com>
2024-10-21 01:26:42 +0900

    refactor(myenv): Change myenv command implementation from bash script to zsh alias&function
    (to add a new sub command "myenv cd")

--- config/home/.config/zsh/_aliases.zsh
+++ config/home/.config/zsh/_aliases.zsh
@@ -39,3 +39,70 @@ alias lg="lazygit"
 alias g="git"
 
 alias v="nvim"
+
+# ======================================================
+# ======== myenv command                               =
+# ======================================================
+
+# What is this?
+#     util CLI for myenv
+#
+# Usage:
+#     subcommands:
+#         apply <hostname>: Run myenv setup as <hostname>
+#         sync: TODO: git pull & git push ?
+#         test: TODO: run staticcheck (lint, format, unit-test, integ-test)
+#
+# Example:
+#     $ myenv cd
+#     $ myenv apply "udon"
+#     $ myenv pull
+#     $ myenv push
+#
+alias myenv="myenv_function $@"
+
+myenv_function() {
+  local -r MYENV_ROOT="${HOME}/.myenv"
+  local -r subcommand="$1"
+  shift # discards the first param ($1) and shifts all other params
+
+  case "$subcommand" in
+  apply)
+    _sub_command_apply "$@"
+    ;;
+  pull)
+    _sub_command_pull
+    ;;
+  push)
+    _sub_command_push
+    ;;
+  test)
+    _sub_command_test
+    ;;
+  *)
+    echo "Unknown subcommand: $subcommand"
+    echo "Usage: $0 {cd|apply|pull|push|test} [arguments]"
+    exit 1
+    ;;
+  esac
+}
+
+_sub_command_apply() {
+  # TODO: Validate arguments?
+  # echo "executing _sub_command_apply with argument: $arg"
+  "${MYENV_ROOT}/setup.bash" "$@"
+}
+
+_sub_command_pull() {
+  cd "$MYENV_ROOT" && git pull
+}
+
+_sub_command_push() {
+  cd "$MYENV_ROOT" && git push
+}
+
+_sub_command_test() {
+  # TODO: Validate arguments?
+  echo "Executing _sub_command_test"
+  echo "TODO: Implement..."
+}
