dc55dc8d9d60100dbe39c812655f14d3ad2deda6
rnazmo <rnazmo@gmail.com>
2026-03-22 05:18:43 +0900

    feat(fcitx5): Add support for syncing Mozc config file

--- recipes/_arch_based_x64/1_base.bash
+++ recipes/_arch_based_x64/1_base.bash
@@ -161,7 +161,9 @@ __setup_fcitx5_config() {
     link_file "${MYENV_ROOT}/config/home/.config/fcitx5/config" "${HOME}/.config/fcitx5/config"
     link_file "${MYENV_ROOT}/config/home/.config/fcitx5/profile" "${HOME}/.config/fcitx5/profile"
     remove_unused_config "${HOME}/.config/fcitx5/conf/xim.conf"
+    remove_unused_config "${HOME}/.config/fcitx5/conf/mozc.conf"
     link_file "${MYENV_ROOT}/config/home/.config/fcitx5/conf/xim.conf" "${HOME}/.config/fcitx5/conf/xim.conf"
+    link_file "${MYENV_ROOT}/config/home/.config/fcitx5/conf/mozc.conf" "${HOME}/.config/fcitx5/conf/mozc.conf"
     remove_file_as_root "/etc/profile.d/fcitx.sh"
     copy_file_as_root "${MYENV_ROOT}/config/etc/profile.d/fcitx.sh" "/etc/profile.d/fcitx.sh"
 }
