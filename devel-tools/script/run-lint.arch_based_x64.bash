9d6998bc6b5670f6dfd0e60bd43dd505111dd353
rnazmo <rnazmo@gmail.com>
2026-06-26 04:25:24 +0900

    style(run-lint): fix variable name typo TARGETSS -> TARGETS

--- devel-tools/script/run-lint.arch_based_x64.bash
+++ devel-tools/script/run-lint.arch_based_x64.bash
@@ -14,7 +14,7 @@ readonly DEVEL_TOOLS_BIN_DIR="${MYENV_ROOT}/devel-tools/bin"
 readonly SHELLCHECK_CMD="shellcheck"
 readonly SHELLCHECK_CMD_PATH="${DEVEL_TOOLS_BIN_DIR}/${SHELLCHECK_CMD}"
 
-readonly TARGETSS=(
+readonly TARGETS=(
     "${MYENV_ROOT}"/*.bash
     "${MYENV_ROOT}"/devel-tools/script/*.bash
     "${MYENV_ROOT}"/hosts/*/*.bash
@@ -24,7 +24,7 @@ readonly TARGETSS=(
 main() {
     log_debug "SHELLCHECK_CMD_PATH: ${SHELLCHECK_CMD_PATH}"
 
-    for TARGETS in "${TARGETSS[@]}"; do
+    for TARGETS in "${TARGETS[@]}"; do
         for TARGET in $TARGETS; do
             log_info "TARGET: $TARGET"
             "$SHELLCHECK_CMD_PATH" "$TARGET"
